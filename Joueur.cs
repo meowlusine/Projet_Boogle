e2ac4726644ea7956f5d6a379800a74eb1dc4c78
Tash-29 <tashhsbnck@gmail.com>
2024-11-17 16:25:54 +0100

    Add files via upload

--- Joueur.cs
+++ Joueur.cs
@@ -8,5 +8,56 @@ namespace Projet_Boogle
 {
     internal class Joueur
     {
+        private string nom,
+        private int score;
+        private Dictionary<string, int> motsTrouves;
+
+        public Joueur(string nom, int score, Dictionary<string, int> motsTrouves) 
+        {
+            this.nom = nom;
+            if (nom=="")    //la condition permet de verifier si le nom entre est null ou vide cf learn microsoft pour autres options
+            {
+                Console.WriteLine("Vous devez avoir un nom")
+                return;
+            }
+            this.score = score;
+            this.motsTrouves = motsTrouves;
+        }
+
+
+        public bool Contain(string mot)
+        {
+            if (motsTrouves.ContainsKey(mot))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Add_Mot(string mot)
+        {
+            if (motsTrouves.ContainsKey(mot) == false) // rajouter la condition 'si mot appartient au dictionnaire
+            {
+                motsTrouves.Add(mot, 1);     // rajouter occurrence du mot
+               
+            }
+            else
+            {
+                motsTrouves[mot] += 1;
+            }
+        }
+
+        public string toString()
+        {
+           Console.Write("Le score de" + this.nom + " est de " + this.score + " grâce aux mots cités suivants");
+           foreach(string mot in motsTrouves)
+            {
+                Console.Write($"{mot}");
+            } 
+
+        }
     }
 }
