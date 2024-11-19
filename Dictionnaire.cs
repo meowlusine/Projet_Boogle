1150c1d9c1c939c52ac775491315f21720e464dd
Mélusine PULIERO <96616995+meowlusine@users.noreply.github.com>
2024-11-19 16:03:00 +0100

    Add files via upload

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -8,5 +8,105 @@ namespace Projet_Boogle
 {
     internal class Dictionnaire
     {
+        private string langue = "fr";
+        private List<string> liste_mots = new List<string>();
+
+        #region Constructeur
+        public Dictionnaire(string langue)
+        {
+            this.langue = langue;
+            List<string> liste_mots = Program.transformation_Dico(langue);
+            liste_mots = Program.tri_fusion(liste_mots);
+            this.liste_mots = liste_mots;
+        }
+        #endregion
+
+        #region Propriétés
+        public List<string> Liste_mots
+        {
+            get { return this.liste_mots; }
+        }
+
+        #endregion
+
+        #region Méthodes
+        public string toString()
+        {
+            Dictionary<int, int> nb_mots_longueur = new Dictionary<int, int>();
+            Dictionary<string, int> nb_mots_lettre = new Dictionary<string, int>();
+
+            foreach (string mot in this.liste_mots)
+            {
+                int taille = mot.Length;
+                if (taille >= 1)
+                {
+                    string lettre = Convert.ToString(mot[0]);
+                    if (nb_mots_longueur.ContainsKey(taille))
+                    {
+                        nb_mots_longueur[taille]++;
+                    }
+                    else
+                    {
+                        nb_mots_longueur.Add(taille, 1);
+                    }
+                    if (nb_mots_lettre.ContainsKey(lettre))
+                    {
+                        nb_mots_lettre[lettre]++;
+                    }
+                    else
+                    {
+                        nb_mots_lettre.Add(lettre, 1);
+                    }
+                }    
+            }
+            string res = "Nombre de mots par lettre : \n";
+            foreach (string lettre in nb_mots_lettre.Keys)
+            {
+                res += lettre + " : " + nb_mots_lettre[lettre] + "\n";
+            }
+            res += "\nNombre de mots par taille : \n";
+            foreach (int taille in nb_mots_longueur.Keys)
+            {
+                res += taille + " : " + nb_mots_longueur[taille] + "\n";
+            }
+
+            return res + "\nLangue : " + this.langue;
+        }
+
+
+        public bool RechDicoRecursif(string mot, List<string> liste_mots)
+        {
+            int milieu = liste_mots.Count / 2;
+            string mot_milieu = liste_mots[milieu];
+
+            if (string.Compare(mot,mot_milieu)==0)
+            {
+                return true; 
+            }
+            else if(liste_mots.Count == 0 || liste_mots.Count ==1)
+            {
+                return false;
+            }
+            else
+            {
+                
+                if (string.Compare(mot, mot_milieu) < 0)
+                {
+                    List<string> gauche = liste_mots.GetRange(0, milieu-1);
+                    return RechDicoRecursif(mot,gauche);
+                }
+                else if(string.Compare(mot, mot_milieu) > 0)
+                {
+                    List<string> droite = liste_mots.GetRange(milieu,liste_mots.Count - milieu);
+                    return RechDicoRecursif(mot, droite);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        #endregion
     }
+
 }
