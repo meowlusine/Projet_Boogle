533d5778bb91e727f54031b6809d27a89c0680c7
Tash-29 <tashhsbnck@gmail.com>
2024-12-19 17:52:56 +0100

    Add files via upload

--- Joueur.cs
+++ Joueur.cs
@@ -9,6 +9,9 @@ using System.Drawing.Imaging;
 
 namespace Projet_Boogle
 {
+    /// <summary>
+    /// classe permet de créer un joueur
+    /// </summary>
     internal class Joueur
     {
         private string nom;
@@ -18,7 +21,7 @@ namespace Projet_Boogle
         public Joueur(string nom)
         {
             this.nom = nom;
-            if (nom == "")    //la condition permet de verifier si le nom entre est null ou vide cf learn microsoft pour autres options
+            if (nom == "")    
             {
                 Console.WriteLine("Vous devez avoir un nom");
                 return;
@@ -40,6 +43,13 @@ namespace Projet_Boogle
             get { return this.motsTrouves;}
             set { this.motsTrouves=value;}
             }
+
+
+        /// <summary>
+        /// La fonction Contain permet de déterminer si le mot entré a déjà été trouvé par le joueur
+        /// </summary>
+        /// <param name="mot"></param>
+        /// <returns> un booléen qui renvoie true si le mot a déjà été trouvé par le joueur, false sinon</returns>                  
         public bool Contain(string mot)
         {
             if (motsTrouves.ContainsKey(mot))
@@ -51,12 +61,18 @@ namespace Projet_Boogle
                 return false;
             }
         }
-
+        /// <summary>
+        /// la focntion Add_Mot ajoute le mot entré en paramètre à la liste des mots trouvés 
+        /// s'il n'a pas encore été trouvé, s'il a déjà été trouvé lors d'un tours précédent, 
+        /// son occurrence augmente de 1
+        /// enfin, on ajoute les points gagnés par ce mot au score total du joueur
+        /// </summary>
+        /// <param name="mot"></param>
         public void Add_Mot(string mot)
         {
-            if (motsTrouves.ContainsKey(mot) == false) // rajouter la condition 'si mot appartient au dictionnaire
+            if (motsTrouves.ContainsKey(mot) == false) 
             {
-                motsTrouves.Add(mot, 1);     // rajouter occurrence du mot
+                motsTrouves.Add(mot, 1);     
                
             }
             else
@@ -66,6 +82,14 @@ namespace Projet_Boogle
             this.score += ScoreDuMot(mot);
         }
 
+        
+        /// <summary>
+        /// Dans la class Jeu, nous avons un dictionnaire contenant les lettres en clé et une matrice contenant le poids
+        /// et leur occurence. Pour chaque lettre du mot, on récupère le poids coorespondant et on l'additionne au 
+        /// score du mot
+        /// </summary>
+        /// <param name="mot"></param>
+        /// <returns>le score du mot en fonction du poids de chaque lettre</returns>
         public int ScoreDuMot(string mot)
         {
             int scoreMot = 0;
@@ -78,10 +102,14 @@ namespace Projet_Boogle
             return scoreMot;
         }
 
+        /// <summary>
+        /// une méthode indiquant le nom du joueur, so score et les mots trouvés
+        /// </summary>
+        /// <returns>un string avec les informations demandées</returns>
         public string toString()
         {
             string res = "Le score de " + this.nom + " est de " + this.score + " grâce aux mots cités suivants \n"; 
-           foreach(string mot in motsTrouves.Keys)
+            foreach(string mot in motsTrouves.Keys)
             {
                 res += mot+", ";
             }
@@ -94,18 +122,18 @@ namespace Projet_Boogle
         /// </summary>
         public void GenererNuageDeMots()
         {
-            // Dimensions de l'image du nuage de mots
+           
             int largeur = 800;
             int hauteur = 600;
 
-            // Créer l'image avec les dimensions spécifiées
+            
             Bitmap image = new Bitmap(largeur, hauteur);
             Graphics g = Graphics.FromImage(image);
 
-            // Définir le fond blanc
+            
             g.Clear(Color.White);
 
-            // Choisir une police de base et des couleurs dans les tons violets et roses
+           
             Random rand = new Random();
             System.Drawing.Font[] polices = {
         new System.Drawing.Font("Arial", 12),
@@ -116,54 +144,54 @@ namespace Projet_Boogle
     };
 
             Brush[] couleurs = {
-        new SolidBrush(Color.FromArgb(128, 0, 128)), // Violet
-        new SolidBrush(Color.FromArgb(186, 85, 211)), // Orchidée
-        new SolidBrush(Color.FromArgb(138, 43, 226)), // Bleu violet
-        new SolidBrush(Color.FromArgb(255, 20, 147)), // Deep pink (rose profond)
-        new SolidBrush(Color.FromArgb(255, 105, 180)), // Hot pink (rose vif)
-        new SolidBrush(Color.FromArgb(255, 182, 193)), // Rose pâle
-        new SolidBrush(Color.FromArgb(255, 105, 180)), // Rose vif
-        new SolidBrush(Color.FromArgb(219, 112, 147))  // Medium violet red
+        new SolidBrush(Color.FromArgb(128, 0, 128)), 
+        new SolidBrush(Color.FromArgb(186, 85, 211)), 
+        new SolidBrush(Color.FromArgb(138, 43, 226)), 
+        new SolidBrush(Color.FromArgb(255, 20, 147)), 
+        new SolidBrush(Color.FromArgb(255, 105, 180)), 
+        new SolidBrush(Color.FromArgb(255, 182, 193)), 
+        new SolidBrush(Color.FromArgb(255, 105, 180)), 
+        new SolidBrush(Color.FromArgb(219, 112, 147))  
     };
 
-            // Liste pour enregistrer les positions des mots
+           
             List<Rectangle> positions = new List<Rectangle>();
 
-            // Pour éviter la superposition, on va d'abord générer les tailles de mots et leur fréquence
+            
             List<KeyValuePair<string, int>> motsFrequences = MotsTrouves.ToList();
-            motsFrequences.Sort((x, y) => y.Value.CompareTo(x.Value)); // Trier par fréquence décroissante
+            motsFrequences.Sort((x, y) => y.Value.CompareTo(x.Value)); 
 
-            // Variables pour essayer de placer les mots sans chevauchement
-            int maxTentatives = 200; // Augmenter les tentatives pour une meilleure chance de placement
-            int minSpacing = 10; // Espacement minimum entre les mots
+            
+            int maxTentatives = 200; 
+            int minSpacing = 10; 
 
             foreach (var mot in motsFrequences)
             {
-                // La taille de la police est basée sur la fréquence du mot
-                int taillePolice = 40 + mot.Value * 17; // La taille varie en fonction de la fréquence
+               
+                int taillePolice = 40 + mot.Value * 17; 
                 System.Drawing.Font font = new System.Drawing.Font("Arial", taillePolice);
 
-                // Choisir une couleur au hasard parmi celles définies
+                
                 Brush couleur = couleurs[rand.Next(couleurs.Length)];
 
-                // Calculer la largeur et la hauteur du mot avec cette police
+               
                 SizeF tailleMot = g.MeasureString(mot.Key, font);
                 int motLargeur = (int)tailleMot.Width;
                 int motHauteur = (int)tailleMot.Height;
 
-                // Essayer de positionner le mot sans chevauchement
+                
                 bool trouvePlace = false;
                 int tentatives = 0;
 
                 while (!trouvePlace && tentatives < maxTentatives)
                 {
-                    // Générer une position aléatoire proche des autres mots
+                    
                     int x = rand.Next(minSpacing, largeur - motLargeur - minSpacing);
                     int y = rand.Next(minSpacing, hauteur - motHauteur - minSpacing);
 
                     Rectangle position = new Rectangle(x, y, motLargeur, motHauteur);
 
-                    // Vérifier si le mot chevauche un mot déjà placé
+                    
                     bool overlap = false;
                     foreach (var pos in positions)
                     {
@@ -176,35 +204,35 @@ namespace Projet_Boogle
 
                     if (!overlap)
                     {
-                        // Appliquer la transformation (horizontal ou vertical)
-                        int rotationAngle = rand.Next(0, 2) == 0 ? 0 : 90; // 0° pour horizontal, 90° pour vertical
-                        g.TranslateTransform(position.X + motLargeur / 2, position.Y + motHauteur / 2); // Déplacer le centre
-                        g.RotateTransform(rotationAngle); // Rotation du graphique
+                        
+                        int rotationAngle = rand.Next(0, 2) == 0 ? 0 : 90; 
+                        g.TranslateTransform(position.X + motLargeur / 2, position.Y + motHauteur / 2); 
+                        g.RotateTransform(rotationAngle); 
 
-                        // Dessiner le mot avec la position et la rotation
-                        g.DrawString(mot.Key, font, couleur, new PointF(-motLargeur / 2, -motHauteur / 2)); // Position relative au centre
+                       
+                        g.DrawString(mot.Key, font, couleur, new PointF(-motLargeur / 2, -motHauteur / 2)); 
 
-                        // Restaurer la transformation (annuler la rotation et translation)
-                        g.RotateTransform(-rotationAngle); // Annuler la rotation
-                        g.TranslateTransform(-(position.X + motLargeur / 2), -(position.Y + motHauteur / 2)); // Annuler la translation
+                        
+                        g.RotateTransform(-rotationAngle); 
+                        g.TranslateTransform(-(position.X + motLargeur / 2), -(position.Y + motHauteur / 2)); 
 
-                        positions.Add(position); // Ajouter la position du mot
-                        trouvePlace = true; // Le mot est placé sans chevauchement
+                        positions.Add(position); 
+                        trouvePlace = true; 
                     }
 
                     tentatives++;
                 }
             }
 
-            // Enregistrer l'image dans un fichier
+           
             string path = $"{Nom}_nuage_mots.png";
             image.Save(path, ImageFormat.Png);
            
 
-            // Ouvrir l'image automatiquement
+            
             System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(path) { UseShellExecute = true });
 
-            // Libérer les ressources
+            
             g.Dispose();
             image.Dispose();
         }
